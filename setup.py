cfc9cd8c542527ce0af628e5c460d84382d8c038
Vinod kumar Yerrabalali <Vinodkumar9676150@gmail.com>
2026-01-02 16:22:04 +0530

    update the requirement.txt files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='USA_Visa',
+    version='0.0.0',
+    author='Vinod Kumar',
+    author_email='vinodkumar9676150@gmail.com',
+    packages=find_packages(),
+)
